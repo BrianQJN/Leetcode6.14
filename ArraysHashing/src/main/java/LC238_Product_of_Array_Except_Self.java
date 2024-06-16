eb3bc9d49cef205f614f023dd4e07b235c893db6
BrianQu <qujianning0401@163.com>
2024-06-16 12:52:26 +0800

    feature: LC238 Product of Array Except Self

--- ArraysHashing/src/main/java/LC238_Product_of_Array_Except_Self.java
+++ ArraysHashing/src/main/java/LC238_Product_of_Array_Except_Self.java
@@ -31,4 +31,25 @@ public class LC238_Product_of_Array_Except_Self {
 
         return res;
     }
+
+    public int[] productExceptSelf2(int[] nums) {
+        int n = nums.length;
+
+        int[] res = new int[n];
+
+        res[0] = 1;
+        // left products
+        for (int i = 1; i < n; i++) {
+            res[i] = res[i-1] * nums[i-1];
+        }
+
+        // right products
+        int rightProduct = 1;
+        for (int j = n - 1; j >= 0; j--) {
+            res[j] = res[j] * rightProduct;
+            rightProduct *= nums[j];
+        }
+
+        return res;
+    }
 }
---
+++ ArraysHashing/src/main/java/LC36_Valid_Sudoku.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Brian Qu
+ * @version 1.0
+ * @implSpec
+ * @since 2024-06-16
+ */
+public class LC36_Valid_Sudoku {
+    public boolean isValidSudoku(char[][] board) {
+        Set<String> block
+    }
+}
