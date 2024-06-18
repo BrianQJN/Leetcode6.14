0afc26a4a598ab7d81bd6aaf849008626ffc3ca9
BrianQu <qujianning0401@163.com>
2024-06-18 17:40:30 +0800

    feature: LC704 Binary Search

---
+++ BinarySearch/src/main/java/LC704_Binary_Search.java
@@ -0,0 +1,29 @@
+/**
+ * @author Brian Qu
+ * @version 1.0
+ * @implSpec
+ * @since 2024-06-18
+ */
+public class LC704_Binary_Search {
+    public int search(int[] nums, int target) {
+        // initialize two pointers
+        int left = 0, right = nums.length - 1;
+        int mid = 0;
+
+        // binary search
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+
+            // mid > target, left interval, mid < target, right interval
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+}
