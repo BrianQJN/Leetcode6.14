c1057cbfa06867090c792b5bf7f58b5fb8e23943
BrianQu <qujianning0401@163.com>
2024-06-23 21:14:18 +0800

    feature: LC2 Add Two Numbers

--- LinkedList/src/main/java/LC2_Add_Two_Numbers.java
+++ LinkedList/src/main/java/LC2_Add_Two_Numbers.java
@@ -5,5 +5,31 @@
  * @since 2024-06-21
  */
 public class LC2_Add_Two_Numbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        // initialize dummy node and a carry digit
+        ListNode dummy = new ListNode(-1);
+        ListNode cur = dummy;
+        int carry = 0;
 
+        // iterate through each digit
+        while (l1 != null || l2 != null || carry != 0) {
+            int sum = carry;
+
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            carry = sum / 10;
+            cur.next = new ListNode(sum % 10);
+            cur = cur.next;
+        }
+
+        return dummy.next;
+    }
 }
