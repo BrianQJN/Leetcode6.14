d35e8c67981b161c4e3479e138c897c9efc379b6
BrianQu <qujianning0401@163.com>
2024-06-29 13:46:24 -0400

    feature: LC98 Validate Binary Search Tree

---
+++ Tree/src/main/java/LC98_Validate_Binary_Search_Tree.java
@@ -0,0 +1,23 @@
+/**
+ * @author Brian Qu
+ * @version 1.0
+ * @implSpec
+ * @since 2024-06-29
+ */
+public class LC98_Validate_Binary_Search_Tree {
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private boolean isValidBST(TreeNode node, long lower, long upper) {
+        if (node == null) {
+            return true;
+        }
+
+        if (node.val <= lower || node.val >= upper) {
+            return false;
+        }
+
+        return isValidBST(node.left, lower, node.val) && isValidBST(node.right, node.val, upper);
+    }
+}
