e053aa5ac73ddb1216c8efeb65a0f1e922e753ae
BrianQu <qujianning0401@163.com>
2024-07-03 16:10:54 -0400

    feature: LC208 Implement Trie(Prefix Tree)

---
+++ Tries/src/main/java/LC208_Implement_Trie.java
@@ -0,0 +1,57 @@
+/**
+ * @author Brian Qu
+ * @version 1.0
+ * @implSpec
+ * @since 2024-07-03
+ */
+public class LC208_Implement_Trie {
+    private class TrieNode {
+        private TrieNode[] children;
+        private boolean isEndOfWord;
+
+        public TrieNode() {
+            children = new TrieNode[26];
+            isEndOfWord = false;
+        }
+    }
+
+    private TrieNode root;
+
+    public LC208_Implement_Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode cur = root;
+        for (char c : word.toCharArray()) {
+            int idx = c - 'a';
+            if (cur.children[idx] == null) {
+                cur.children[idx] = new TrieNode();
+            }
+            cur = cur.children[idx];
+        }
+        cur.isEndOfWord = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode node = searchPrefix(word);
+        return node != null && node.isEndOfWord;
+    }
+
+    private TrieNode searchPrefix(String prefix) {
+        TrieNode cur = root;
+        for (char c : prefix.toCharArray()) {
+            int idx = c - 'a';
+            if (cur.children[idx] == null) {
+                return null;
+            }
+            cur = cur.children[idx];
+        }
+
+        return cur;
+    }
+
+    public boolean startsWith(String prefix) {
+        return searchPrefix(prefix) != null;
+    }
+}
