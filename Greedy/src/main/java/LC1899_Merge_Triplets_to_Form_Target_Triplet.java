28abb0da7a1a8a2d813d31ff98f62b9fe7fc688e
BrianQu <qujianning0401@163.com>
2024-07-17 23:40:37 -0400

    feature: LC1899 Merge Triplets to Form Target Triplet

---
+++ Greedy/src/main/java/LC1899_Merge_Triplets_to_Form_Target_Triplet.java
@@ -0,0 +1,11 @@
+/**
+ * @author Brian Qu
+ * @version 1.0
+ * @implSpec
+ * @since 2024-07-17
+ */
+public class LC1899_Merge_Triplets_to_Form_Target_Triplet {
+    public boolean mergeTriplets(int[][] triplets, int[] target) {
+
+    }
+}
