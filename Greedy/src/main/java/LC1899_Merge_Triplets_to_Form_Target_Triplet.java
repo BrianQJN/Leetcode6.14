1a1b61125f9856a232b37cf5de17992ba3fa1b2e
BrianQu <qujianning0401@163.com>
2024-07-18 00:18:42 -0400

    feature: LC1899 Merge Triplets to Form Target Triplet

--- Greedy/src/main/java/LC1899_Merge_Triplets_to_Form_Target_Triplet.java
+++ Greedy/src/main/java/LC1899_Merge_Triplets_to_Form_Target_Triplet.java
@@ -6,6 +6,16 @@
  */
 public class LC1899_Merge_Triplets_to_Form_Target_Triplet {
     public boolean mergeTriplets(int[][] triplets, int[] target) {
+        boolean[] canAchieve = new boolean[3];
 
+        for (int[] triplet : triplets) {
+            if (triplet[0] <= target[0] && triplet[1] <= target[1] && triplet[2] <= target[2]) {
+                canAchieve[0] |= (triplet[0] == target[0]);
+                canAchieve[1] |= (triplet[1] == target[1]);
+                canAchieve[2] |= (triplet[2] == target[2]);
+            }
+        }
+
+        return canAchieve[0] && canAchieve[1] && canAchieve[2];
     }
 }
